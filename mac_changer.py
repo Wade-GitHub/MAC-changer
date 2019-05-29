9641ce4ddeaf7bede9499196d84aa54052573d15
Wade <wwrwade@gmail.com>
2019-05-29 15:32:46 +1200

    Updated mac_changer.py and cleaned up code.

--- mac_changer.py
+++ mac_changer.py
@@ -30,19 +30,30 @@ def change_mac(interface, new_mac):
     subprocess.run(["ifconfig", interface, "up"])
 
 
-options = get_arguments()
-#change_mac(options.interface, options.new_mac)
+def get_current_mac(interface):
+    # check_output() will run the command and return its output
+    # can save the output to a variable to print
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
 
-# check_output() will run the command and return its output
-# can save the output to a variable to print
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-# have to convert bytes object to string
-print(str(ifconfig_result, 'utf-8'))
+    # using regex to find mac address pattern in ifconfig command output
+    mac_address_search_result = re.search(r"(\w{2}:){5}\w{2}", str(ifconfig_result, 'utf-8'))
+    # check that the interface we gave can have a mac address
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
 
-# using regex to find mac address pattern in ifconfig command output
-mac_address_search_result = re.search(r"(\w{2}:){5}\w{2}", str(ifconfig_result, 'utf-8'))
-# check that the interface we gave can have a mac address
-if mac_address_search_result:
-    print(mac_address_search_result.group(0))
-else:
-    print("[-] Could not read MAC address.")
+
+if __name__ == "__main__":
+    options = get_arguments()
+
+    current_mac = get_current_mac(options.interface)
+    print(f"Current MAC = {current_mac}")
+
+    change_mac(options.interface, options.new_mac)
+
+    current_mac = get_current_mac(options.interface)
+    if current_mac == options.new_mac:
+        print(f"[+] MAC address was successfully changed to {current_mac}")
+    else:
+        print("[-] MAC address did not get changed.")
